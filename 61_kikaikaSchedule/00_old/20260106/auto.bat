fcc60ae8307c6a2aa51db87519911760d93d43fa
Koya Chida <is31160177@icloud.com>
2026-01-14 13:24:40 +0900

    Delete 61_kikaikaSchedule directory

--- 61_kikaikaSchedule/00_old/20260106/auto.bat
+++
@@ -1,8 +0,0 @@
-﻿@echo off
-cd C:\Users\di2413\Desktop\MyPython\61_kikaikaSchedule
-python assyExcelInport.py
-python AssyScheduleUpdate.py
-python child_requirements.py
-python kikaikaSchedule.py
-python run_and_mail.py
-
--- 61_kikaikaSchedule/auto.bat
+++
@@ -1,7 +0,0 @@
-﻿@echo off
-cd C:\Users\di2413\Desktop\MyPython\61_kikaikaSchedule
-python assyExcelInport.py
-python AssyScheduleUpdate.py
-python child_requirements.py
-python master_kikaika.py
-
